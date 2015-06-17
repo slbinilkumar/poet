89161290aa213931fcfcdd4125d7bd77eef6e3d9
Jose Sotelo <rdz.sotelo@gmail.com>
2015-06-17 14:58:51 -0400

    Transition is now a stacked lstm. Also, change in name.

--- poet.py
+++ poet.py
@@ -1,38 +1,31 @@
 import numpy
 from numpy import array
-import ipdb
 
 import theano
 from theano import tensor
 
+from blocks.algorithms import (GradientDescent, Scale,
+                               StepClipping, CompositeRule)
 from blocks.bricks import Tanh, Initializable
 from blocks.bricks.base import application
 from blocks.bricks.lookup import LookupTable
-from blocks.bricks.recurrent import SimpleRecurrent, LSTM
-
+from blocks.bricks.recurrent import LSTM, RecurrentStack
 from blocks.bricks.sequence_generators import (
     SequenceGenerator, Readout, SoftmaxEmitter, LookupFeedback)
+
 from blocks.config import config
+from blocks.extensions import FinishAfter, Printing
+from blocks.extensions.saveload import SimpleExtension
+from blocks.extensions.monitoring import TrainingDataMonitoring
 from blocks.graph import ComputationGraph
-from fuel.transformers import Mapping, Batch, Padding, Filter
-from fuel.datasets import OneBillionWord, TextFile
-from fuel.schemes import ConstantScheme
-from blocks.dump import load_parameter_values
-from blocks.algorithms import (GradientDescent, Scale,
-                               StepClipping, CompositeRule)
-from blocks.initialization import Orthogonal, IsotropicGaussian, Constant
+from blocks.initialization import IsotropicGaussian, Constant
+from blocks.main_loop import MainLoop
 from blocks.model import Model
 from blocks.monitoring import aggregation
 
-from blocks.extensions import FinishAfter, Timing
-from blocks.extensions.saveload import Checkpoint
-
-from blocks.main_loop import MainLoop
-from blocks.filter import VariableFilter
-from blocks.utils import named_copy, dict_union
-
-from blocks.extensions import FinishAfter, Printing, Timing
-from blocks.extensions.saveload import SimpleExtension
+from fuel.datasets import OneBillionWord, TextFile
+from fuel.transformers import Mapping, Batch, Padding, Filter
+from fuel.schemes import ConstantScheme
 
 config.recursion_limit = 100000
 floatX = theano.config.floatX
@@ -54,17 +47,19 @@ def _transpose(data):
 def _filter_long(data):
     return len(data[0]) <= 100
 
-class Parrot(Initializable):
-    """Parrot is your favourite text-generation pet.
-    Parrot should learn your favourite topics, and talk to you about them.
+class Poet(Initializable):
+    """Poet is your favourite text-generation artist.
+    Poet should learn your favourite topics, and talk to you about them.
     """
-    def __init__(self, dimension, alphabet_size, **kwargs):
-        super(Parrot, self).__init__(**kwargs)
+    def __init__(self, dimension, depth, alphabet_size, **kwargs):
+        super(Poet, self).__init__(**kwargs)
 
         lookup = LookupTable(alphabet_size, dimension)
 
-        transition = LSTM(
-            dim=dimension, name="transition")
+        recurrents = [LSTM(dim=dimension) for _ in range(depth)]
+
+        transition = RecurrentStack( recurrents,
+            name="transition", skip_connections = True)
 
         readout = Readout(
             readout_dim=alphabet_size,
@@ -87,21 +82,21 @@ class Parrot(Initializable):
         return aggregation.mean(self.generator.cost_matrix(
             chars, chars_mask).sum(), chars.shape[1])
 
-class Sample(SimpleExtension):
-    """Make your parrot talk
+class Write(SimpleExtension):
+    """Make your poet write
     Parameters
     ----------
     chars : int Number of characters to generate
     """
-    def __init__(self, parrot, chars=100, **kwargs):
-        super(Sample, self).__init__(**kwargs)
+    def __init__(self, poet, chars=100, **kwargs):
+        super(Write, self).__init__(**kwargs)
         steps = 300
-        sample = ComputationGraph(parrot.generator.generate(n_steps=steps, 
+        sample = ComputationGraph(poet.generator.generate(n_steps=steps, 
             batch_size=1, iterate=True))
         self.sample_fn = sample.get_theano_function()
 
     def do(self, callback_name, *args):
-        _1, _2, outputs, _3 = self.sample_fn()
+        _1, _2, _3, _4, _5, _6, outputs, _8 = self.sample_fn()
 
         try:
             true_length = list(outputs).index(array(char2code['</S>'])) + 1
@@ -112,14 +107,14 @@ class Sample(SimpleExtension):
         print "".join(code2char[code[0]] for code in outputs)
 
 
-parrot = Parrot(dimension = 2000, alphabet_size = len(char2code), name="parrot")
-parrot.weights_init = IsotropicGaussian(0.1)
-parrot.biases_init = Constant(0.0)
+poet = Poet(dimension = 200, depth = 3, alphabet_size = len(char2code), name="poet")
+poet.weights_init = IsotropicGaussian(0.1)
+poet.biases_init = Constant(0.0)
 
 dataset_options = dict(dictionary=char2code, level="character",
                                preprocess=_lower)
 
-#dataset = TextFile(data_path, **dataset_options)
+#dataset = TextFile(["poet.py"], **dataset_options)
 dataset = OneBillionWord("training", [99], **dataset_options)
 
 data_stream = dataset.get_example_stream()
@@ -132,7 +127,7 @@ data_stream = Mapping(data_stream, _transpose)
 chars = tensor.lmatrix("features")
 chars_mask = tensor.matrix("features_mask")
 
-cost  = parrot.cost(chars, chars_mask)
+cost  = poet.cost(chars, chars_mask)
 cost.name = "sequence_log_likelihood"
 
 model = Model(cost)
@@ -146,10 +141,19 @@ algorithm = GradientDescent(
     cost=cost, params=cg.parameters,
     step_rule=CompositeRule([StepClipping(10.0), Scale(0.01)]))
 
+monitor = TrainingDataMonitoring(
+    variables=[cost],
+    after_epoch=True,
+    prefix="test")
+
 main_loop = MainLoop(
     model=model,
     data_stream=data_stream,
     algorithm=algorithm,
-    extensions=[Sample(parrot = model.get_top_bricks()[0],every_n_batches = 100)])
+    extensions=[
+    monitor,
+    Printing(),
+    Write(poet = model.get_top_bricks()[0],after_epoch = True)
+    ])
 
 main_loop.run()
